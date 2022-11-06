a6e6c1ae6374b49238014e423401d4eaf696419e
abdulalimsujon <alimsujon12@gmail.com>
2022-11-06 22:29:28 +0600

    first assignment t

---
+++ index.js
@@ -0,0 +1,11 @@
+import { users } from './userData.js'
+
+users.map((user) => {
+    console.log(`the user name is :   ${user.name}`)
+    console.log(`the user email :        ${user.email}`)
+    console.log(' ')
+})
+
+
+
+
